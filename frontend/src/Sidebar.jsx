24d0bd0a1c6fd20bf541e98179f97074e47e38bb
varad-desaavle <desavalevarad1@gmail.com>
2026-07-15 12:59:13 +0530

    Updated frontend for Vercel deployment

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar() {
 
     const getAllThreads = async () => {
         try{
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/thread`);
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
 
